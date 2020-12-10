1d49d4fc15e9920e68d5f2821fac13f37d4744d7
shaniakiat <kiat.shania@gmail.com>
2020-12-09 23:43:19 -0500

    gsap animation

---
+++ index.js
@@ -0,0 +1,55 @@
+gsap.registerPlugin(ScrollTrigger);
+var tl = gsap.timeline();
+
+tl.from(".content", {
+  y: "-30%",
+  opacity: 0,
+  duration: 2,
+  ease: Power4.easeOut,
+});
+
+tl.from(
+  ".stagger1",
+  {
+    opacity: 0,
+    y: -50,
+    stagger: 0.3,
+    ease: Power4.easeOut,
+    duration: 2,
+  },
+  "-=1.5"
+);
+tl.from(
+  ".hero-design",
+  {
+    opacity: 0,
+    y: 50,
+    ease: Power4.easeOut,
+    duration: 1,
+  },
+  "-=2"
+);
+
+tl.to(".scroll", { y: 15, repeat: -1, yoyo: true }, "-=1.2");
+
+gsap.from(".transition2", {
+  scrollTrigger: {
+    trigger: ".transition2",
+    start: "top bottom",
+  },
+  y: 50,
+  opacity: 0,
+  duration: 1.2,
+  stagger: 0.3,
+});
+
+gsap.from(".transition3", {
+  scrollTrigger: {
+    trigger: ".transition3",
+    start: "top bottom",
+  },
+  y: 50,
+  opacity: 0,
+  duration: 1.2,
+  stagger: 0.6,
+});
